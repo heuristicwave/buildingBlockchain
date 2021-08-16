682af19618a2db8ace39afc68b9755518d3258bd
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-08-16 21:33:05 +0900

    Create genesisBlock struct

--- main.go
+++ main.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"crypto/sha256"
 	"fmt"
-
-	"githun.com/heuristicwave/buildingBlockchain/person"
 )
 
+type block struct {
+	data     string
+	hash     string
+	prevHash string
+}
+
 func main() {
-	my := person.Person{}
-	my.SetDetails("heuri", 7)
-	fmt.Println("Main : ", my)
+	genesisBlock := block{"Genesis Block", "", ""}
+	hash := sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash))
+	hexHash := fmt.Sprintf("%x", hash)
+	genesisBlock.hash = hexHash
+	fmt.Println(genesisBlock)
 }
