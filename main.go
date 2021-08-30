3ed8cc9ce09aa13fa4b49d87f81a853dab83f364
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-08-30 23:49:50 +0900

    Setup go webserver

--- main.go
+++ main.go
@@ -2,18 +2,19 @@ package main
 
 import (
 	"fmt"
-
-	"githun.com/heuristicwave/buildingBlockchain/blockchain"
+	"log"
+	"net/http"
 )
 
+const port string = ":4000"
+
+// homeHandler
+func home(rw http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(rw, "Hello from Server!") // write on Response
+}
+
 func main() {
-	chain := blockchain.GetBlockchain()
-	chain.AddBlock("Second Block")
-	chain.AddBlock("Third Block")
-	chain.AddBlock("Fourth Block")
-	for _, block := range chain.AllBlocks() {
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %s\n", block.Hash)
-		fmt.Printf("Prev Hash: %s\n", block.PrevHash)
-	}
+	http.HandleFunc("/", home) // route
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
