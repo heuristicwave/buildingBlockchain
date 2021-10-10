86a5fa1bbabc9402e94d20d2b2d436116d4f30c7
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-10-10 21:19:38 +0900

    Add Rendering Templates

--- main.go
+++ main.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
+
+	"githun.com/heuristicwave/buildingBlockchain/blockchain"
 )
 
 const port string = ":4000"
 
+type homeData struct {
+	PageTitle string // 대,소문자 (public, private)
+	Blocks    []*blockchain.Block
+}
+
 // homeHandler
 func home(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Hello from Server!") // write on Response
+	// ParseFiles 는 에러를 return 하므로 같이 지정하는데,
+	// Must를 사용하여 if err!=nil 까지 대체
+	tmpl := template.Must(template.ParseFiles("templates/home.html"))
+	// Template에 Handler에서 만든 data 전달
+	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
+	tmpl.Execute(rw, data)
 }
 
 func main() {
