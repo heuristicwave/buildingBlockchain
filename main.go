00e30c3e0e1d6a79c25a6b81e65da3037b2a43d9
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-10-13 22:37:32 +0900

    Refactoring explorer

--- main.go
+++ main.go
@@ -1,50 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"html/template"
-	"log"
-	"net/http"
-
-	"githun.com/heuristicwave/buildingBlockchain/blockchain"
-)
-
-const (
-	port        string = ":4000"
-	templateDir string = "templates/"
-)
-
-var templates *template.Template
-
-type homeData struct {
-	PageTitle string // 대,소문자 (public, private)
-	Blocks    []*blockchain.Block
-}
-
-// homeHandler
-func home(rw http.ResponseWriter, r *http.Request) {
-	// Template에 Handler에서 만든 data 전달
-	data := homeData{"Home", blockchain.GetBlockchain().AllBlocks()}
-	templates.ExecuteTemplate(rw, "home", data)
-}
-
-func add(rw http.ResponseWriter, r *http.Request) {
-	switch r.Method {
-	case "GET":
-		templates.ExecuteTemplate(rw, "add", nil)
-	case "POST":
-		r.ParseForm()
-		data := r.Form.Get("blockData")
-		blockchain.GetBlockchain().AddBlock(data)
-		http.Redirect(rw, r, "/", http.StatusPermanentRedirect)
-	}
-}
+import "githun.com/heuristicwave/buildingBlockchain/explorer"
 
 func main() {
-	templates = template.Must(template.ParseGlob(templateDir + "pages/*.gohtml"))
-	templates = template.Must(templates.ParseGlob(templateDir + "partials/*.gohtml"))
-	http.HandleFunc("/", home) // route
-	http.HandleFunc("/add", add)
-	fmt.Printf("Listening on http://localhost%s\n", port)
-	log.Fatal(http.ListenAndServe(port, nil))
+	explorer.Start()
 }
