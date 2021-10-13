41618aedd7db431170daf13766105164a61d848d
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-10-14 01:30:19 +0900

    Add Sending JSON

--- main.go
+++ main.go
@@ -1,7 +1,38 @@
 package main
 
-import "githun.com/heuristicwave/buildingBlockchain/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"githun.com/heuristicwave/buildingBlockchain/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+// Sending JSON our users
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	jsonByte, err := json.Marshal(data) // (byte, error)
+	utils.HandleErr(err)
+	fmt.Printf("%s", jsonByte)
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
