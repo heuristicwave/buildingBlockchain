815a4bcfc5a211a381d27dbc1d6843346db554ff
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-10-18 11:22:20 +0900

    Add ServerMux

---
+++ rest/rest.go
@@ -0,0 +1,85 @@
+package rest
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"githun.com/heuristicwave/buildingBlockchain/blockchain"
+	"githun.com/heuristicwave/buildingBlockchain/utils"
+)
+
+var port string
+
+type url string
+
+func (u url) MarshalText() ([]byte, error) {
+	url := fmt.Sprintf("http://localhost%s%s", port, u)
+	return []byte(url), nil
+}
+
+// struct field tag, omitempty : hide unless object
+type urlDescription struct {
+	URL         url    `json:"url"`
+	Method      string `json:"method"`
+	Description string `json:"description"`
+	Payload     string `json:"payload,omitempty"`
+}
+
+type AddBlockBody struct {
+	Message string
+}
+
+// Sending JSON our users
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []urlDescription{
+		{
+			URL:         url("/"),
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+		{
+			URL:         url("/blocks"),
+			Method:      "GET",
+			Description: "See All Blocks",
+		},
+		{
+			URL:         url("/blocks"),
+			Method:      "POST",
+			Description: "Add A Block",
+			Payload:     "data:string",
+		},
+		{
+			URL:         url("/blocks/{id}"),
+			Method:      "GET",
+			Description: "See A Block",
+		},
+	}
+	// text/plain => json
+	rw.Header().Add("Content-Type", "application/json")
+	// Use NewEncoder instead of Marshal
+	json.NewEncoder(rw).Encode(data)
+}
+
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
+func Start(aPort int) {
+	handler := http.NewServeMux()
+	port = fmt.Sprintf(":%d", aPort)
+	handler.HandleFunc("/", documentation)
+	handler.HandleFunc("/blocks", blocks)
+	fmt.Printf("Listening on http://localhost%s\n", port)
+	log.Fatal(http.ListenAndServe(port, handler))
+}
