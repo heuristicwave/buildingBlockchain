0c26d2641ca823b091d0840cc688c1c98b5428b9
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-10-20 18:22:38 +0900

    Add Blocks height

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"githun.com/heuristicwave/buildingBlockchain/explorer"
 	"githun.com/heuristicwave/buildingBlockchain/rest"
 )
 
 func main() {
-	go explorer.Start(3000)
+	// go explorer.Start(3000)
 	rest.Start(4000)
 }
--- rest/rest.go
+++ rest/rest.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	"githun.com/heuristicwave/buildingBlockchain/blockchain"
@@ -52,7 +53,7 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Payload:     "data:string",
 		},
 		{
-			URL:         url("/blocks/{id}"),
+			URL:         url("/blocks/{height}"),
 			Method:      "GET",
 			Description: "See A Block",
 		},
@@ -78,7 +79,10 @@ func blocks(rw http.ResponseWriter, r *http.Request) {
 
 func block(rw http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r) // fmt.Println(vars) => map[id:X]
-	id := vars["id"]
+	id, err := strconv.Atoi(vars["height"])
+	utils.HandleErr(err)
+	block := blockchain.GetBlockchain().GetBlock(id)
+	json.NewEncoder(rw).Encode(block)
 }
 
 func Start(aPort int) {
@@ -86,7 +90,7 @@ func Start(aPort int) {
 	port = fmt.Sprintf(":%d", aPort)
 	router.HandleFunc("/", documentation).Methods("GET")
 	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
-	router.HandleFunc("/blocks/{id:[0-9]+}", block).Methods("GET")
+	router.HandleFunc("/blocks/{height:[0-9]+}", block).Methods("GET")
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, router))
 }
