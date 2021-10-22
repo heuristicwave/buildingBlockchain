2313ec76ea0c2d35cec3091cd67ad3b66a0a6fdb
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-10-22 14:09:17 +0900

    Add Middleware

--- rest/rest.go
+++ rest/rest.go
@@ -62,8 +62,6 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Description: "See A Block",
 		},
 	}
-	// text/plain => json
-	rw.Header().Add("Content-Type", "application/json")
 	// Use NewEncoder instead of Marshal
 	json.NewEncoder(rw).Encode(data)
 }
@@ -71,7 +69,6 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 func blocks(rw http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "GET":
-		rw.Header().Add("Content-Type", "application/json")
 		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
 	case "POST":
 		var addBlockBody AddBlockBody
@@ -95,9 +92,18 @@ func block(rw http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(rw).Encode(block)
 }
 
+// Adapter Pattern
+func jsonContentTypeMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(rw http.ResponseWriter, r *http.Request) {
+		rw.Header().Add("Content-Type", "application/json")
+		next.ServeHTTP(rw, r)
+	})
+}
+
 func Start(aPort int) {
-	router := mux.NewRouter()
 	port = fmt.Sprintf(":%d", aPort)
+	router := mux.NewRouter()
+	router.Use(jsonContentTypeMiddleware)
 	router.HandleFunc("/", documentation).Methods("GET")
 	router.HandleFunc("/blocks", blocks).Methods("GET", "POST")
 	router.HandleFunc("/blocks/{height:[0-9]+}", block).Methods("GET")
