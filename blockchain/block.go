c829191d4c75de4b8129e0ce2db6997260d72796
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-11-04 10:54:38 +0900

    Add Hash Utils

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,9 +1,7 @@
 package blockchain
 
 import (
-	"crypto/sha256"
 	"errors"
-	"fmt"
 	"strings"
 
 	"githun.com/heuristicwave/buildingBlockchain/db"
@@ -44,9 +42,7 @@ func FindBlock(hash string) (*Block, error) {
 func (b *Block) mine() {
 	target := strings.Repeat("0", b.Difficulty)
 	for {
-		blockAsString := fmt.Sprint(b)
-		hash := fmt.Sprintf("%x", sha256.Sum256([]byte(blockAsString)))
-		fmt.Printf("Block as String:%s\nHash:%s\nTarget:%s\nNonce:%d\n\n\n", blockAsString, hash, target, b.Nonce)
+		hash := utils.Hash(b)
 		if strings.HasPrefix(hash, target) {
 			b.Hash = hash
 			break
