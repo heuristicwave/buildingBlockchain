530241cdbc9a9f96ae340e4b46561f36d4177896
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-11-30 18:17:59 +0900

    Temp Channel Test

--- main.go
+++ main.go
@@ -1,11 +1,34 @@
 package main
 
 import (
-	"githun.com/heuristicwave/buildingBlockchain/cli"
-	"githun.com/heuristicwave/buildingBlockchain/db"
+	"fmt"
+	"time"
 )
 
+// Only Send Channel
+func countToTen(c chan<- int) {
+	for i := range [10]int{} {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("sending %d\n", i)
+		c <- i // send i to channel
+	}
+	close(c) // avoid deadlock
+}
+
+// Only Receive Channel
+func receive(c <-chan int) {
+	for {
+		a, ok := <-c
+		if !ok {
+			fmt.Println("Done")
+			break
+		}
+		fmt.Printf("received %d\n", a)
+	}
+}
+
 func main() {
-	defer db.Close()
-	cli.Start()
+	c := make(chan int)
+	go countToTen(c)
+	receive(c)
 }
