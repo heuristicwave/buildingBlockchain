d5c9e5b7c98643a49d5e87cd0da4f4aa116df3fd
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-12-06 20:09:26 +0900

    Add Peer other client

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/websocket"
 	"githun.com/heuristicwave/buildingBlockchain/utils"
@@ -13,25 +12,16 @@ var conns []*websocket.Conn
 var upgrader = websocket.Upgrader{}
 
 func Upgrade(rw http.ResponseWriter, r *http.Request) {
+	// Port(3000) will upgrade the request from port(4000)
 	upgrader.CheckOrigin = func(r *http.Request) bool {
 		return true
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
-	conns = append(conns, conn) // connect with other client
 	utils.HandleErr(err)
-	for {
-		_, p, err := conn.ReadMessage()
-		if err != nil {
-			break
-		}
-		fmt.Printf("Just got:%s\n\n", p)
-		time.Sleep(5 * time.Second)
-		message := fmt.Sprintf("New message: %s", p)
-		utils.HandleErr(conn.WriteMessage(websocket.TextMessage, []byte(message)))
-		for _, aConn := range conns {
-			if aConn != conn {
-				utils.HandleErr(aConn.WriteMessage(websocket.TextMessage, p))
-			}
-		}
-	}
+}
+
+func AddPeer(address, port string) {
+	// Dial : Port(4000) is requesting an upgrade from the port(3000)
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws", address, port), nil)
+	utils.HandleErr(err)
 }
