e874c6b4e9620af879a2163bc974517e697c9da8
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-12-12 17:45:28 +0900

    Test Read Peer

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,6 +3,7 @@ package p2p
 import (
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/websocket"
 	"githun.com/heuristicwave/buildingBlockchain/utils"
@@ -21,6 +22,8 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
 	initPeer(conn, ip, openPort)
+	time.Sleep(10 * time.Second)
+	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 3000!"))
 }
 
 func AddPeer(address, port, openPort string) {
@@ -28,4 +31,6 @@ func AddPeer(address, port, openPort string) {
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
 	initPeer(conn, address, port)
+	time.Sleep(5 * time.Second)
+	conn.WriteMessage(websocket.TextMessage, []byte("Hello from Port 4000!"))
 }
