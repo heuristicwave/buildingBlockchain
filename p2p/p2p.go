cdad8b5eeddc9df47df4f87de121bacc70a1585e
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-12-19 15:13:48 +0900

    Add Broadcast Newest Block

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -3,7 +3,6 @@ package p2p
 import (
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/websocket"
 	"githun.com/heuristicwave/buildingBlockchain/utils"
@@ -21,16 +20,13 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	}
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
-	peer := initPeer(conn, ip, openPort)
-	time.Sleep(10 * time.Second)
-	peer.inbox <- []byte("Hello from 3000!")
+	initPeer(conn, ip, openPort)
 }
 
 func AddPeer(address, port, openPort string) {
 	// Dial : Port(4000) is requesting an upgrade from the port(3000)
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
-	peer := initPeer(conn, address, port)
-	time.Sleep(5 * time.Second)
-	peer.inbox <- []byte("Hello from 4000!")
+	p := initPeer(conn, address, port)
+	sendNewestBlock(p)
 }
