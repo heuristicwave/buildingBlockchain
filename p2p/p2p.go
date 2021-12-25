bdd1f3183e890e2beaa8eaffac96b6dbf787a04b
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2021-12-26 00:00:26 +0900

    Enable sync block each node

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -18,6 +18,7 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 	upgrader.CheckOrigin = func(r *http.Request) bool {
 		return openPort != "" && ip != ""
 	}
+	fmt.Printf("%s wants an upgrade\n", openPort)
 	conn, err := upgrader.Upgrade(rw, r, nil)
 	utils.HandleErr(err)
 	initPeer(conn, ip, openPort)
@@ -25,6 +26,7 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 
 func AddPeer(address, port, openPort string) {
 	// Dial : Port(4000) is requesting an upgrade from the port(3000)
+	fmt.Printf("%s wants to connect to port %s\n", openPort, port)
 	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
 	utils.HandleErr(err)
 	p := initPeer(conn, address, port)
