10a7842c702bb0422335c0c6fab7566cc48350d6
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2022-01-02 22:25:27 +0900

    Add Global Memory Pool

--- blockchain/transactions.go
+++ blockchain/transactions.go
@@ -14,7 +14,7 @@ const (
 )
 
 type mempool struct {
-	Txs []*Tx
+	Txs map[string]*Tx
 	m   sync.Mutex
 }
 
@@ -24,7 +24,9 @@ var memOnce sync.Once
 
 func Mempool() *mempool {
 	memOnce.Do(func() {
-		m = &mempool{}
+		m = &mempool{
+			Txs: make(map[string]*Tx),
+		}
 	})
 	return m
 }
@@ -155,15 +157,18 @@ func (m *mempool) AddTx(to string, amount int) (*Tx, error) {
 	if err != nil {
 		return nil, err
 	}
-	m.Txs = append(m.Txs, tx)
+	m.Txs[tx.ID] = tx
 	return tx, nil
 }
 
 func (m *mempool) TxToConfirm() []*Tx {
 	coinbase := makeCoinbaseTx(wallet.Wallet().Address)
-	txs := m.Txs
+	var txs []*Tx
+	for _, tx := range m.Txs {
+		txs = append(txs, tx)
+	}
 	txs = append(txs, coinbase)
-	m.Txs = nil
+	m.Txs = make(map[string]*Tx)
 	return txs
 }
 
@@ -171,5 +176,5 @@ func (m *mempool) AddPeerTx(tx *Tx) {
 	m.m.Lock()
 	defer m.m.Unlock()
 
-	m.Txs = append(m.Txs, tx)
+	m.Txs[tx.ID] = tx
 }
