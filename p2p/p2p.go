3ddeb2147178f3a2d4a1db8b11981a131d167683
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2022-01-04 10:31:50 +0900

    Edit port parsing

--- p2p/p2p.go
+++ p2p/p2p.go
@@ -5,8 +5,8 @@ import (
 	"net/http"
 
 	"github.com/gorilla/websocket"
-	"githun.com/heuristicwave/buildingBlockchain/blockchain"
-	"githun.com/heuristicwave/buildingBlockchain/utils"
+	"github.com/heuristicwave/buildingBlockchain/blockchain"
+	"github.com/heuristicwave/buildingBlockchain/utils"
 )
 
 var conns []*websocket.Conn
@@ -28,10 +28,10 @@ func Upgrade(rw http.ResponseWriter, r *http.Request) {
 func AddPeer(address, port, openPort string, broadcast bool) {
 	// Dial : Port(4000) is requesting an upgrade from the port(3000)
 	fmt.Printf("%s wants to connect to port %s\n", openPort, port)
-	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort[1:]), nil)
+	conn, _, err := websocket.DefaultDialer.Dial(fmt.Sprintf("ws://%s:%s/ws?openPort=%s", address, port, openPort), nil)
 	utils.HandleErr(err)
 	p := initPeer(conn, address, port)
-	if !broadcast {
+	if broadcast {
 		broadcastNewPeer(p)
 		return
 	}
