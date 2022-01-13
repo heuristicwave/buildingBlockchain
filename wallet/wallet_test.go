20633a2e458a7554121c4fc4435ab30d8d2dba01
heuristicwave <31366038+heuristicwave@users.noreply.github.com>
2022-01-13 16:29:28 +0900

    Create wallet_test.go

---
+++ wallet/wallet_test.go
@@ -0,0 +1,30 @@
+package wallet
+
+import (
+	"crypto/x509"
+	"encoding/hex"
+	"testing"
+)
+
+const (
+	testKey     string = "307702010104209ab1bf01ef131b9b40201f80f73e8a322fa1bc93c0f0face07b9b983e5ea2620a00a06082a8648ce3d030107a14403420004a58855a07e004d7579faedee60806d589e4155a4c1c9dda6b2ced1ab8263592f697e73a031d42975837ddde456fa901d28c9c26bfaee2a5ba82686773b162626"
+	testPayload string = "009040c3c2b4bf56f7ad8eaf2c5b5f78ca29bde0855716f7160442196c8a88d5"
+	testSig     string = "e78eaf52a1d07b92a92dcd2555d21b324023cf06bcd1bb13af0888ab18b3af8b6616c1c5cf31486a4ed9c3c3a13a2f5b4a39c97e002744d1b1deb62e3ba44e0b"
+)
+
+func makeTestWallet() *wallet {
+	w := &wallet{}
+	b, _ := hex.DecodeString(testKey)
+	key, _ := x509.ParseECPrivateKey(b)
+	w.privateKey = key
+	w.Address = aFromK(key)
+	return w
+}
+
+func TestSign(t *testing.T) {
+	s := Sign(testPayload, makeTestWallet())
+	_, err := hex.DecodeString(s)
+	if err != nil {
+		t.Errorf("Sign() should return a hex encoded string, got %s", s)
+	}
+}
